8de305f80972ce11918983a27b41d1a11815c609
Rumyantsev Artem <rumartcer@gmail.com>
2020-02-13 16:16:57 +0500

    Some changes for testing #3

--- compiler/scripts/generateFile.js
+++ compiler/scripts/generateFile.js
@@ -4,6 +4,6 @@ const toTemplate = require('lodash').template
 module.exports = (writekey, port) => {
   const template = fs.readFileSync(__dirname + '/../src/index.js.tpl').toString('utf8');
   const compiled = toTemplate(template);
-  const host = 'http://beta-02.carrotquest.io/segment/analytics.js'
+  const host = `'http://beta-02.carrotquest.io/segment/analytics.js'`
   fs.writeFileSync(__dirname + '/../src/index.html', compiled({ host: host, writekey: writekey }))
 }
